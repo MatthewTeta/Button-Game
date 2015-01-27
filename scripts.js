70e0a2be7cc84b31a626a00b3e7e22b4d73adf8d
MatthewTeta <randommagicmatt@gmail.com>
2015-01-27 11:26:02 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,11 +4,20 @@ $(document).ready(function() {
     $(this).css('width', size + 'px');
     $(this).css('height', size + 'px');
     $(this).css('border-radius', Math.floor(Math.random() * 100)) + '%');
-    $(this).css('background-color', 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255  + ')');
+    $(this).css('top',Math.floor(Math.random() * $(window).height()));
+    $(this).css('left',Math.floor(Math.random() * $(window).width()));
   });
   
   $('.button').mouseover(function() {
+    var size = Math.floor(Math.random() * 100);
+    $(this).css('width', size + 'px');
+    $(this).css('height', size + 'px');
+    $(this).css('border-radius', Math.floor(Math.random() * 100)) + '%');
     $(this).css('top',Math.floor(Math.random() * $(window).height()));
     $(this).css('left',Math.floor(Math.random() * $(window).width()));
+  });
+  
+  $('.button').click(function() {
+    $(this).css('background-color', "#00AAFF");
   });
 });
