d0e6dd5df0d8f314b2d994cccf6473ba8a684088
MatthewTeta <matthewtetadev@gmail.com>
2015-01-28 10:39:26 -0700

    test

--- scripts.js
+++ scripts.js
@@ -3,16 +3,12 @@ $(document).ready(function() {
     var size = Math.floor(Math.random() * 100);
     $(this).css('width', size + 'px');
     $(this).css('height', size + 'px');
-    $(this).css('border-radius', Math.floor(Math.random() * 100)) + '%');
+    $(this).css('border-radius', Math.floor(Math.random() * 100) + '%');
     $(this).css('top',Math.floor(Math.random() * $(window).height()));
     $(this).css('left',Math.floor(Math.random() * $(window).width()));
   });
   
   $('.button').mouseover(function() {
-    var size = Math.floor(Math.random() * 100);
-    $(this).css('width', size + 'px');
-    $(this).css('height', size + 'px');
-    $(this).css('border-radius', Math.floor(Math.random() * 100)) + '%');
     $(this).css('top',Math.floor(Math.random() * $(window).height()));
     $(this).css('left',Math.floor(Math.random() * $(window).width()));
   });
